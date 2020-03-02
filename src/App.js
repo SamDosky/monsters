62fa0295f7db14ce1097c91ac3251a256c833604
Sami Alaa <alaaalsudani@ac.c4q.nyc>
2020-03-01 21:28:08 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ export default class App extends Component{
     constructor(){
         super()
     this.state = {
-        monsters: []
+        monsters: [],
+        search: ''
     }   
 }
 
@@ -16,11 +17,20 @@ componentDidMount(){
     .then(users => this.setState({monsters: users}))
 }
 
+searchMonster = (e) =>{
+  this.setState({
+    search : e.target.value
+  })
+}
+
 render(){
+const {monsters , search} =this.state
+
+const filtermonster = monsters.filter(monster => monster.name.toLowerCase().includes(search.toLowerCase()))
  return(
      <div>
-       <CardList monsters={this.state.monsters}>
-         
+       <input type = 'search' onChange={this.searchMonster}/>
+       <CardList monsters={filtermonster}>
        </CardList>
      </div>
  )}
